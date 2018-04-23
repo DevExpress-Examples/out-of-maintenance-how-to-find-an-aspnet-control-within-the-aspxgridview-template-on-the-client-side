5b7e3e8e6b1641259b88a39e316fa2cb3ee1427b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,17 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Web.Services
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Public Function getClientID(ByVal colName As String, ByVal controlName As String) As String
+
+		Dim t As TextBox = CType(grid.FindHeaderTemplateControl(grid.Columns(colName), controlName), TextBox)
+		Return t.ClientID
+	End Function
+End Class
